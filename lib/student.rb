ef49f357f9d63eda834dd0198fc946eff4b27bbc
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-09 17:08:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
    end
 
    def self.new_from_db(row)
-      new_student = self.new  # self.new is the same as running Song.new
+      new_student = self.new  # self.new is the same as running Student.new
       new_student.id = row[0]
       new_student.name =  row[1]
       new_student.grade = row[2]
