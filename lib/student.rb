f03686a49a0d53fd870e97cabf05cfb30090e74a
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-09 17:02:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,18 @@ class Student
       new_student  # return the newly created instance
     end
 
+    def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+        sql = <<-SQL
+          SELECT *
+          FROM students
+          WHERE name = ?
+        SQL
+        row = DB[:conn].execute(sql)
+        self.new_from_db(row[0])
+      end
+
 
 
 
