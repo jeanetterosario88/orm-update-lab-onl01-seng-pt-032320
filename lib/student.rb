4b71ebda09496821e742c26159c2d8674f1d66c3
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-09 18:15:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,10 +47,11 @@ class Student
    end
 
    def self.new_from_db(row)
-      new_student = self.new(name, grade)  # self.new is the same as running Student.new
+      # self.new is the same as running Student.new
       new_student.id = row[0]
       new_student.name =  row[1]
       new_student.grade = row[2]
+      new_student = self.new(name, grade)
       new_student  # return the newly created instance
     end
 
