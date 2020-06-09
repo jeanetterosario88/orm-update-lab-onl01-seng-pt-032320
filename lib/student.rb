9928fe8a788850f63a5a134e248eaad9e5aaba35
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-09 16:42:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,16 +2,27 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
   def initialize(name, grade, id = nil)
-  @id = id
-  @name = name
-  @album = album
-end
-def initialize(id=nil, name, album)
-  @id = id
-  @name = name
-  @album = album
-end
+    @id = id
+    @name = name
+    @grade = album
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+
 
 
 
