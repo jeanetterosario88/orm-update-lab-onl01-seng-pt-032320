8b2fcf538ec275d49bca87c775789f5751e3a3eb
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-09 16:39:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,17 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade, id = nil)
+  @id = id
+  @name = name
+  @album = album
+end
+def initialize(id=nil, name, album)
+  @id = id
+  @name = name
+  @album = album
+end
+
 
 
 end
